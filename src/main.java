1a3c123b1cdde92c7e2a3777c8ff68d1ea5785ca
Ashira <timecrisis1995@live.com>
2017-10-28 15:38:23 +0700

    Update -convert every line to 32bit

--- src/main.java
+++ src/main.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
  */
 public class main {
     public static void main(String[] args) throws IOException {
+        String a = "";
         //----Read file and input each line in array-list----//
         Scanner file = new Scanner(new File("output.txt"));
         List<String> line = new ArrayList<>();
@@ -16,7 +17,19 @@ public class main {
         String[] mem = line.toArray(new String[0]); //Add every lines form array-list to array
         //---------------------------------------------------//
         for(int i = 0 ; i < mem.length ; i++){
-            System.out.println(mem[i]);
+            a = toBinary(Integer.toBinaryString(Integer.parseInt(mem[i])));
+            System.out.println(a);
         }
+    }
+
+    public static String toBinary(String int1){
+        int count;
+        String tmp = int1;
+        count = tmp.length();
+        while (count < 32) {
+            tmp = "0" + tmp;
+            count++;
+        }
+        return tmp;
     }
 }
