41b18e1f69eeb9ac002550d559770b230121eebb
Ashira <timecrisis1995@live.com>
2017-10-28 16:39:59 +0700

    Update -extract op rs rt

--- src/main.java
+++ src/main.java
@@ -8,8 +8,14 @@ import java.util.Scanner;
  * Created by ASUS on 10/27/2017.
  */
 public class main {
+
     public static void main(String[] args) throws IOException {
-        String a = "";
+        String bin ;
+        String rs;
+        String rt;
+        String rd;
+        String op;
+
         //----Read file and input each line in array-list----//
         Scanner file = new Scanner(new File("output.txt"));
         List<String> line = new ArrayList<>();
@@ -17,8 +23,14 @@ public class main {
         String[] mem = line.toArray(new String[0]); //Add every lines form array-list to array
         //---------------------------------------------------//
         for(int i = 0 ; i < mem.length ; i++){
-            a = toBinary(Integer.toBinaryString(Integer.parseInt(mem[i])));
-            System.out.println(a);
+            bin = toBinary(Integer.toBinaryString(Integer.parseInt(mem[i])));
+
+            op = bin.substring(7,10);
+            rs = bin.substring(10,13);
+            rt = bin.substring(13,16);
+
+            System.out.println("opcode : " + op + " rs : " + rs + " rt : " + rt);
+            //System.out.println("op code : " + op + "bin left : " + bin);
         }
     }
 
