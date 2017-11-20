8f748293b2df183ac3f2b7a843e49020f1ead7a1
Ashira <timecrisis1995@live.com>
2017-11-20 13:53:26 +0700

    Almost done except -nand -sw -jarl -noop
    work but didn't check bug
    -add
    -lw
    -beq
    -halt

--- src/main.java
+++ src/main.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.regexp.internal.RE;
+import com.sun.org.apache.xpath.internal.functions.FuncSubstring;
 import instruction.add;
 import instruction.nand;
 
@@ -23,6 +24,7 @@ public class main {
         String offset;
         int state=0;
 
+        int state_num=1;
         int Register[] = new int[8];
 
         //Rule 7(simulator) Initialize all Register to 0
@@ -36,20 +38,21 @@ public class main {
         List<String> line = new ArrayList<>();
         while (file.hasNextLine()){line.add(file.nextLine());}
         String[] mem = line.toArray(new String[0]); //Add every lines form array-list to array
+        for (int i=0;i<mem.length;i++) {
+            System.out.println("mem[ "+i+" ] " + mem[i]);
+        }
         //---------------------------------------------------//
         for(; state < mem.length ; state++){
-            System.out.println("@@@");
-            System.out.println("state:");
-            System.out.println("    pc " + state);
+            printState(mem,Register,state);
+
             bin = toBinary(Integer.toBinaryString(Integer.parseInt(mem[state])));
 
             op = bin.substring(7,10);
 
 
-
             if ((Integer.parseInt(mem[state]) > -32768 && (Integer.parseInt(mem[state])) < 32767))
             {
-                System.out.println("fill : " + Integer.parseInt(mem[state]));
+                break;
             }
             else {
                 switch (op){
@@ -97,13 +100,12 @@ public class main {
                         rs = bin.substring(10,13);
                         rt = bin.substring(13,16);
                         offset = bin.substring(16,32);
-/*
-                        
-                        if(Register[convertBinarytoDecimal(rs)] == Register[convertBinarytoDecimal(rt)]) System.out.println("beq");//state = state + convertBinarytoDecimal(offset);
-                        else {System.out.println("bnq");
-                            break;}
-                            */
-                        break;
+                        //System.out.println(bin.substring(16,32));
+                        //offset = convertBinarytoDecimal(bin.substring(16,32));
+
+                        if(Register[convertBinarytoDecimal(rs)] == Register[convertBinarytoDecimal(rt)]) state = state + convertBinarytoDecimal(offset);
+                        else break;
+
                     case "101" :
                         //test = "jalr";
                         rs = bin.substring(10,13);
@@ -111,6 +113,10 @@ public class main {
                         break;
                     case "110" :
                         test = "halt";
+                        System.out.println("machine halted");
+                        System.out.println("total of "+ state_num +" instructions executed\n");
+                        System.out.println("final state of machine:\n");
+
                         break;
                     case "111" :
                         test = "noop";
@@ -118,20 +124,15 @@ public class main {
                     default:
                         System.out.println("Error : Put wrong OP CODE");
                 }
-                //System.out.println(test);
-                for (int j = 0; j<mem.length;j++)
-                    System.out.println("    memory[" + j + "]" + "= " + mem[j] );
-                System.out.println("registers:");
-                for (int k= 0; k<Register.length;k++) {
-                    System.out.println("    register[" + k + "]" + "= " + Register[k]);
-                }
+
             }
 
-            System.out.println("end state\n");
 
+            state_num++;
 
 
         }
+
 
     }
 
@@ -143,22 +144,48 @@ public class main {
             tmp = "0" + tmp;
             count++;
         }
+        //System.out.println(tmp);
         return tmp;
     }
     
     public static int convertBinarytoDecimal(String num)
     {
         int dec = 0;
-        int number = Integer.parseInt(num) ;
-        int remainder;
+        long number = Long.parseLong(num,10) ;
+        long remainder;
         for (int i=0;number != 0;i++)
         {
             remainder = number %10;
             number /= 10;
-            dec += remainder* Math.pow(2,i);
+
+            if(i==num.length()-1)dec += (int)remainder* Math.pow(-2,i);
+            else dec += (int)remainder* Math.pow(2,i);
         }
         return dec;
     }
 
+    public static String convertNum(String num)
+    {
+        String tmp=num;
+        for (int i = num.length(); i<32;i++)
+        {
+            tmp = num.substring(0,1) + tmp;
+        }
+
+        return tmp;
+    }
+
+    public static void printState(String []mem,int []Register,int count){
+
+        System.out.println("\n@@@\n" + "\tpc " + count +"\n\tmemory:");
+        for (int i=0;i<mem.length;i++) {
+            System.out.println("\t\tmem[ "+i+" ] " + mem[i]);
+        }
+        System.out.println("\tregister:");
+        for (int i=0;i<Register.length;i++) {
+            System.out.println("\t\treg[ "+i+" ] " + Register[i]);
+        }
+        System.out.println("end state");
+    }
 
 }
